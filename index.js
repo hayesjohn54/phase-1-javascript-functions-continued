c4cf0a5ef4a39ac000e0560b19ac821dde85b64d
John Hayes <hayesjohn54@yahoo.com>
2022-06-07 17:52:32 -0400

    commit tree 14a47b9bb5671f5458a9c57389046d887846e866

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(basketball = "roller-skate"){
+    if ("basketball"){
+    return `This Saturday, I want to ${basketball}!`;
+
+}  }
+
+function mondayWork(work = 'go to the office'){
+    if("work"){
+        return `This Monday, I will ${work}.`;
+    }
+
+}
+
+function wrapAdjective(flair = "*"){
+    return function (inner = `You are ${flair}!`) {
+        return `You are ${flair}${inner}${flair}!`;
+    }
+}
